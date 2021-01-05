0323fc8fca609618028a1bbdea4acf47fa27cf60
zacharytq <zacharytq@gmail.com>
2021-01-04 20:38:42 -0500

    commit tree 65ba3be172658419075ba7ccb447d3e6cc52a801

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+ function mapToNegativize(source){
+     return source.map(x => x - (x*2))
+ }
+
+ function mapToNoChange(source){
+     return source
+ }
+
+ function mapToDouble(source){
+     return source.map(x => x*2)
+ }
+
+ function mapToSquare(source){
+     return source.map(x => x * x)
+ }
+
+ function reduceToTotal(array, start= 0) {
+    const reducer = (accumulator, item) => {
+        return accumulator + item;
+      };
+    return array.reduce(reducer, start)
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce((acc, element) => {
+        if (element === false) {
+            return false
+        } else {
+            return true
+        }
+    })
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce((acc, element) => {
+        if (element === true) {
+            return true
+        } else {
+            return false
+        }
+    })
+}
